f7fab0fead9d6616859326ac9d5688ba41ebca19
David MOLINARI <contact@davidmolinari.fr>
2018-11-19 09:51:27 +0100

    Add Hand template class

---
+++ src/core/Hand.java
@@ -0,0 +1,25 @@
+package core;
+
+import java.util.ArrayList;
+
+public class Hand {
+	
+	private int maxCard;
+	
+	private ArrayList<Card> cards;
+	
+	
+	
+	public void shuffleCards(ArrayList<Card> cards) {
+		//TODO Shuffle ArrayList
+	}
+	
+	public void displayCard(Card card) {
+		//TODO Display a card, meh
+	}
+	
+	public void moveCard(Card card) {
+		//TODO Move a specific card, meh
+	}
+
+}
