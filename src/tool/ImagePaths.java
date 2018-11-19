b07f2389606c0364dc2691acc3df3b058748206f
David Molinari <contact@davidmolinari.fr>
2018-11-19 11:48:05 +0100

    add Heroes

--- src/tool/ImagePaths.java
+++ src/tool/ImagePaths.java
@@ -1,21 +1,21 @@
 package tool;
 
 public class ImagePaths {
-	
-	
+
 	/** SBIRE IMG PATHS **/
-	public static String _SBIRE_3K = "./assets/img/Cards/Sbirs/3K.png"; 
-	public static String _SBIRE_FURRY = "./assets/img/Cards/Sbirs/Furry.png"; 
-	public static String _SBIRE_GAROU = "./assets/img/Cards/Sbirs/Garou.png"; 
-	public static String _SBIRE_IVAN = "./assets/img/Cards/Sbirs/Ivan.png"; 
-	public static String _SBIRE_LAPEYRLE = "./assets/img/Cards/Sbirs/Lapeyrle.png"; 
-	public static String _SBIRE_MAMIE = "./assets/img/Cards/Sbirs/mamie.png"; 
-	public static String _SBIRE_SBIRE_FEMININ= "./assets/img/Cards/Sbirs/SbireFeminin.png";
-	
-	
-	
-	/* HEREO IMG PATHS*/
-	
+	public static String _SBIRE_3K = "./assets/img/Cards/Sbirs/3K.png";
+	public static String _SBIRE_FURRY = "./assets/img/Cards/Sbirs/Furry.png";
+	public static String _SBIRE_GAROU = "./assets/img/Cards/Sbirs/Garou.png";
+	public static String _SBIRE_IVAN = "./assets/img/Cards/Sbirs/Ivan.png";
+	public static String _SBIRE_LAPEYRLE = "./assets/img/Cards/Sbirs/Lapeyrle.png";
+	public static String _SBIRE_MAMIE = "./assets/img/Cards/Sbirs/mamie.png";
+	public static String _SBIRE_SBIRE_FEMININ = "./assets/img/Cards/Sbirs/SbireFeminin.png";
+
+	/* HEREO IMG PATHS */
+	public static String _HERO_PAPOUNOU = "./assets/img/Cards/Heroes/papounou.png";
+	public static String _HERO_CHAUVINATOR = "./assets/img/Cards/Heroes/papounou.png";
+
+	/* HEREO IMG PATHS */
 	public static String _BACKGROUND = "./assets/img/background.jpg";
 
 }
