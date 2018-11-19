4ec4d7515d2cb1274f3d44a968da93e96726702a
David Molinari <contact@davidmolinari.fr>
2018-11-19 11:29:50 +0100

    fix missing constructors

--- src/core/Monster.java
+++ src/core/Monster.java
@@ -1,9 +1,16 @@
 package core;
 
+import java.awt.Image;
+
 public class Monster extends Card implements ICard, IAttackingCard{
 
 	
 	
+	public Monster(String name, String description, int mana, Image image, String effect) {
+		super(name, description, mana, image, effect);
+		// TODO Auto-generated constructor stub
+	}
+
 	public void action() {
 		System.out.println("Monster does smth");
 	}
